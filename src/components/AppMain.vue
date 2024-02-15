9bde6f18c4240871111aeed3f786173f2e847d47
MatteoSanson <matteo.sanson@gmail.com>
2024-02-15 20:45:30 +0100

    feat: milestone5

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,9 +1,14 @@
 <script>
+import ProjectCard from './ProjectCard.vue';
+
 export default {
     name: 'Main',
     props: [
         'projects',
-    ]
+    ],
+    components: {
+        ProjectCard,
+    },
 };
 </script>
 
@@ -16,28 +21,13 @@ export default {
 
             <div class="row">
                 <div class="col col-md-4 g-3" v-for="project in projects">
-                    <div class="card">
-                        <div class="card-body">
-                            <h4 class="card-title custom-h-h4">{{ project.title }}</h4>
-                            <p class="card-text"><strong>Visibility:</strong> {{ project.visibility }}</p>
-                            <p class="card-text"><strong>Type:</strong> {{ project.type.title }}</p>
-                            <p class="card-text"><strong>Technologies:</strong>{{ project.technologies.length > 0 ?
-                                ' ' : ' /' }}
-                                <span v-for="(technology, index) in project.technologies" :key="index">
-                                    {{ technology.title }}{{ index === project.technologies.length - 1 ? '' : ', ' }}
-                                </span>
-                            </p>
-                            <a href="#" class="card-link">Card link</a>
-                        </div>
-                    </div>
+                    <ProjectCard :project="project" />
                 </div>
             </div>
+
+
         </div>
     </main>
 </template>
 
-<style scoped>
-.custom-h-h4 {
-    height: 55px !important;
-}
-</style>
+<style scoped></style>
