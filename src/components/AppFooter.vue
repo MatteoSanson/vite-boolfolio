c96f4868a72003b19fa605c2f87e0ac976888490
MatteoSanson <matteo.sanson@gmail.com>
2024-02-15 18:45:49 +0100

    fix:style

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,15 +1,17 @@
 <script>
+import '../assets/scss/partials/variables.scss';
+
 export default {
     name: 'Footer',
 };
 </script>
 
 <template>
-    <footer>
+    <footer class="bg-custom-h-f">
         <div class="container py-3">
             <h6>Made by classe110</h6>
         </div>
     </footer>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss"></style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,13 @@
 <script>
+import '../assets/scss/partials/variables.scss';
+
 export default {
     name: 'Header',
 };
 </script>
 
 <template>
-    <header>
+    <header class="bg-custom-h-f">
         <div class="container text-center py-3">
             <h1>
                 Laravel-ViteVue Boolfolio
@@ -14,4 +16,4 @@ export default {
     </header>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss"></style>
