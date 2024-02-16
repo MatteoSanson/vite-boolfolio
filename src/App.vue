1545a331171e6c2e8704757fcd373cb1e6c3d2c5
MatteoSanson <matteo.sanson@gmail.com>
2024-02-16 15:59:30 +0100

    fix: import and components

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,13 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
-import AppMain from './components/AppMain.vue';
 import AppFooter from './components/AppFooter.vue';
-import axios from 'axios';
 
 export default {
   name: 'Boolfolio',
-  data() {
-    return {
-      projects: [],
-      startUrl: 'http://127.0.0.1:8000',
-      apiUrls: {
-        projects: '/api/projects',
-      }
-    }
-  },
   components: {
     AppHeader,
-    AppMain,
     AppFooter
   },
-  methods: {
-    getProjects() {
-      axios.get(this.startUrl + this.apiUrls.projects).then(response => {
-        console.log(response);
-        this.projects = response.data.results.data;
-      }).catch(error => { console.log(error) });
-    },
-  },
-  created() {
-    this.getProjects();
-  }
 };
 </script>
 
