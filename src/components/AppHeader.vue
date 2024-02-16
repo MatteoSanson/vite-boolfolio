d495f5d3c35359970630e13ac0752a02d38c7194
MatteoSanson <matteo.sanson@gmail.com>
2024-02-16 15:48:12 +0100

    feat: router link

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -8,10 +8,20 @@ export default {
 
 <template>
     <header class="bg-custom-h-f">
-        <div class="container text-center py-3">
-            <h1>
-                Laravel-ViteVue Boolfolio
-            </h1>
+        <div class="container d-flex justify-content-between py-3 align-items-center">
+            <div class="col-2">Logo</div>
+            <div>
+                <h1>
+                    Laravel-ViteVue Boolfolio
+                </h1>
+            </div>
+            <nav class="col-2">
+                <ul class="list-unstyled m-0 p-0 gap-4 d-flex">
+                    <li><router-link :to="{ name: 'home' }">Home</router-link></li>
+                    <li><router-link :to="{ name: 'projects' }">Projects</router-link></li>
+                    <li><router-link :to="{ name: 'about' }">About</router-link></li>
+                </ul>
+            </nav>
         </div>
     </header>
 </template>
