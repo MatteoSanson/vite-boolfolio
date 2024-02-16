fd66ff52dab11be4bb0fe0de177a3158f8481284
MatteoSanson <matteo.sanson@gmail.com>
2024-02-16 18:44:09 +0100

    feat: component NotFound

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+    name: 'NotFound',
+}
+</script>
+
+<template>
+    <div class="not-found-container d-flex justify-content-center align-items-center">
+        <div class="not-found-content text-center text-white">
+            <h1 class="display-3">Error 404</h1>
+            <p class="display-6">Page not found</p>
+            <p class="leader"><router-link :to="{ name: 'home' }" class="text-white">Torna alla HomePage</router-link>
+            </p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.not-found-container {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.9);
+    z-index: 9999;
+}
+
+.not-found-content {
+    max-width: 600px;
+    padding: 20px;
+}
+</style>
--- src/pages/router.js
+++ src/pages/router.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import AppHome from './AppHome.vue';
 import ProjectList from './ProjectList.vue';
 import AppAbout from './AppAbout.vue';
+import NotFound from './NotFound.vue';
 
 const router = createRouter({
     history: createWebHistory(),
@@ -21,6 +22,11 @@ const router = createRouter({
             path: '/about',
             name: 'about',
             component: AppAbout,
+        },
+        {
+            path: '/:pathMatch(.*)*',
+            name: 'not-found',
+            component: NotFound,
         },
     ]
 });
