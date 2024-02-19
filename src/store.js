ca67d7bcbd11877d395edba4684e438b6fadc3ff
MatteoSanson <matteo.sanson@gmail.com>
2024-02-19 22:12:54 +0100

    feat: store js searchkey

--- src/store.js
+++ src/store.js
@@ -7,7 +7,9 @@ const store = reactive({
             projects: '/api/projects',
         },
     },
-    projects: {},
+    projects: {
+        searchKey: null,
+    },
 });
 
 export default store;
