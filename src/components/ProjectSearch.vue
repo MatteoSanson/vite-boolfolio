3164d171b89946b6525a1512be235cf2701943d4
MatteoSanson <matteo.sanson@gmail.com>
2024-02-19 22:12:41 +0100

    feat: component project search

---
+++ src/components/ProjectSearch.vue
@@ -0,0 +1,34 @@
+<script>
+import store from '../store';
+
+export default {
+    name: 'ProjectSearch',
+    data() {
+        return {
+            store,
+        };
+    },
+    emits: ['searchProject'],
+    methods: {
+        search() {
+            console.log('funziono e cerco');
+            this.$emit('searchProject');
+        }
+    }
+};
+</script>
+
+<template>
+    <section id="project-search">
+        <form action="" @submit.prevent="search">
+            <div class="form-floating mb-3">
+                <input type="text" class="form-control" id="search-ey" placeholder="Search"
+                    v-model="store.projects.searchKey">
+                <label for="search-key">Search</label>
+            </div>
+            <button class="btn btn-primary btn-sm">Search</button>
+        </form>
+    </section>
+</template>
+
+<style scoped lang="scss"></style>
